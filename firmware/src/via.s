cfbffd38d77d8a36ae6b18e0877c768ae27260e5
Joshua M. Thompson <joshua.thompson@gmail.com>
2019-04-07 15:54:33 -0400

    Remove SPI code Update set_led to only affect the LED bit instead of the whole port Add get_console_mode function.

--- firmware/src/via.s
+++ firmware/src/via.s
@@ -9,18 +9,10 @@
         .export via_irq
         .export wait_ms
         .export set_led
-        .export spi_select
-        .export spi_deselect
-        .export spi_transfer
+        .export get_console_mode
 
-        .importzp jiffies
-
-; Port B bit assignments
-SPI_MISO     = $40
-SPI_EN       = $20
-SPI_SS       = $1C
-SPI_MOSI     = $02
-SPI_SCLK     = $01
+PB_ACT_LED = $80
+PB_CONSOLE = $40
 
 ; VIA register numbers
 via_portb  = $00
@@ -43,10 +35,6 @@ via_portax = $0F
 via1_base := $F000
 via2_base := $F010
 
-        .segment "ZEROPAGE"
-
-spi_byte:   .res    1
-
         .segment "LOWROM"
 
         .a8
@@ -64,13 +52,9 @@ via_init:
 
         ; VIA 1 ports A & B
         stz     via1_base+via_porta
-        lda     #$80
-        sta     via1_base+via_portb
-        ;stz     via1_base+via_portb
-        lda     #SPI_EN
-        sta     via1_base+via_ddra
-        ;stz     via1_base+via_ddra
-        lda     #SPI_EN|SPI_SS|SPI_MOSI|SPI_SCLK|$80
+        stz     via1_base+via_portb
+        stz     via1_base+via_ddra
+        lda     #PB_ACT_LED
         sta     via1_base+via_ddrb
 
         ; VIA 2 ports A & B
@@ -89,7 +73,31 @@ via_irq:
  
 @exit:  rts
 
+;;
+; Return the status of the console select jumper. On
+; exit A will be non-zero if the jumper is installed.
+;
+get_console_mode:
+        lda     via1_base+via_portb
+        and     #PB_CONSOLE
+        rts
+
+;;
+; Set the status of the activity LED to the value
+; in A. This should be called with interrupts
+; disabled to avoid corrupting the port B register.
+;
+; Preserves X,Y and trashes A.
+;
 set_led:
+        cmp     #0
+        beq     @clr
+        lda     via1_base+via_portb
+        ora     #PB_ACT_LED
+        sta     via1_base+via_portb
+        rts
+@clr:   lda     via1_base+via_portb
+        and     #~PB_ACT_LED
         sta     via1_base+via_portb
         rts
 
@@ -110,65 +118,4 @@ wait_ms:
 @wait:  lda     via1_base+via_ifr
         and     #$20
         beq     @wait
-        rts
-
-; Perform a single byte transfer to the specified SPI device.
-;
-; Inputs:
-; A = byte to transmit
-;
-; Output:
-; A = byte receiv,ed
-
-spi_transfer:
-        phx
-        sta     spi_byte
-        ldx     #8          ; do 8 bits
-@loop:  lda     via1_base+via_portb
-        and     #<~SPI_MOSI
-        bit     spi_byte    ; bit 7 => neg bit
-        bpl     @out
-        ora     #SPI_MOSI
-@out:   sta     via1_base+via_portb  ; set MOSI
-        inc     via1_base+via_portb  ; raise SCLK
-        clc
-        bit     via1_base+via_portb  ; MISO => overflow
-        bvc     @shift
-        sec 
-@shift: rol     spi_byte    ; rotate MOSI out, and MISO (via carry) in
-        dec     via1_base+via_portb  ; lower SCLK
-        dex
-        bne     @loop
-        plx
-        lda     spi_byte
-        rts
-
-; Open communications to the selected SPI device
-;
-; Inputs:
-; X = device to select (0-4)
-;
-; Outputs:
-; None
-
-spi_select:
-        pha
-        txa
-        and     #7
-        asl
-        asl
-        ora     #SPI_EN
-        ora     via1_base+via_portb
-        sta     via1_base+via_portb
-        pla
-        rts
-
-; Deselect the currently selected SPI device
-
-spi_deselect:
-        pha
-        lda     via1_base+via_portb
-        and     #<~(SPI_EN|SPI_SS)
-        sta     via1_base+via_portb
-        pla
         rts
