5c9af0e52348cf9ff9f6d51339fff368c292cc49
Joshua M. Thompson <joshua.thompson@gmail.com>
2019-05-05 03:24:31 -0400

    Move key codes for A-Z to their lowercase equivalents to facilitate shift/caps handling.

--- avr/key_codes.h
+++ avr/key_codes.h
@@ -31,37 +31,37 @@
 #define KEY_SEMICOLON  0x3B
 #define KEY_EQUALS     0x3D
 
-#define KEY_A          0x41
-#define KEY_B          0x42
-#define KEY_C          0x43
-#define KEY_D          0x44
-#define KEY_E          0x45
-#define KEY_F          0x46
-#define KEY_G          0x47
-#define KEY_H          0x48
-#define KEY_I          0x49
-#define KEY_J          0x4A
-#define KEY_K          0x4B
-#define KEY_L          0x4C
-#define KEY_M          0x4D
-#define KEY_N          0x4E
-#define KEY_O          0x4F
-#define KEY_P          0x50
-#define KEY_Q          0x51
-#define KEY_R          0x52
-#define KEY_S          0x53
-#define KEY_T          0x54
-#define KEY_U          0x55
-#define KEY_V          0x56
-#define KEY_W          0x57
-#define KEY_X          0x58
-#define KEY_Y          0x59
-#define KEY_Z          0x5A
-
 #define KEY_L_BRACKET  0x5B
 #define KEY_BACKSLASH  0x5C
 #define KEY_R_BRACKET  0x5D
 #define KEY_BACKTICK   0x60
+
+#define KEY_A          0x61
+#define KEY_B          0x62
+#define KEY_C          0x63
+#define KEY_D          0x64
+#define KEY_E          0x65
+#define KEY_F          0x66
+#define KEY_G          0x67
+#define KEY_H          0x68
+#define KEY_I          0x69
+#define KEY_J          0x6A
+#define KEY_K          0x6B
+#define KEY_L          0x6C
+#define KEY_M          0x6D
+#define KEY_N          0x6E
+#define KEY_O          0x6F
+#define KEY_P          0x70
+#define KEY_Q          0x71
+#define KEY_R          0x72
+#define KEY_S          0x73
+#define KEY_T          0x74
+#define KEY_U          0x75
+#define KEY_V          0x76
+#define KEY_W          0x77
+#define KEY_X          0x78
+#define KEY_Y          0x79
+#define KEY_Z          0x7A
 
 /* Non-ASCII keys */
 
